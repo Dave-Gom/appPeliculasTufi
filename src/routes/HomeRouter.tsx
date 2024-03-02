e439163f01c6c8941a83acbff92b990656751363
Dave Gomez <davegomez426@gmail.com>
2024-03-02 18:07:46 -0300

    tabbar styles listo

--- src/routes/HomeRouter.tsx
+++ src/routes/HomeRouter.tsx
@@ -8,90 +8,94 @@ import Icon from 'react-native-vector-icons/FontAwesome6';
 
 import Background from '../components/background/Background';
 import Home from '../pages/Home';
-import {styles} from '../themes/styles';
 
 const HomeRouter = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <View style={styles.wrapper}>
-      <Tab.Navigator
-        initialRouteName="HomePage"
-        screenOptions={{
-          headerShown: false,
-          tabBarBackground: () => <Background />,
-          tabBarInactiveTintColor: 'white',
-          tabBarActiveTintColor: 'white',
-        }}>
-        <Tab.Screen
-          name="HomePage"
-          component={Home}
-          options={{
-            title: '',
-            tabBarIcon: ({color, focused}) => (
-              <TouchableOpacity onPress={() => console.log('hola')}>
-                <LinearGradient
-                  colors={focused ? ['#60FFCA', '#19423400'] : ['transparent']}
-                  style={{borderRadius: 40, padding: 1}}
-                  useAngle={true}
-                  angle={140}>
-                  <View
-                    style={[
-                      {
-                        borderRadius: 40,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      },
-                      focused
-                        ? {height: 80, width: 80}
-                        : {
-                            height: 40,
-                            width: 40,
-                          },
-                    ]}>
-                    <Icon name="house" size={25} color={color} solid />
-                  </View>
-                </LinearGradient>
-              </TouchableOpacity>
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="SeachPage"
-          component={() => <></>}
-          options={{
-            title: '',
-            tabBarIcon: ({color, focused}) => (
-              <TouchableOpacity onPress={() => console.log('hola')}>
-                <LinearGradient
-                  colors={focused ? ['#60FFCA', '#19423400'] : ['transparent']}
-                  style={{borderRadius: 40, padding: 1}}
-                  useAngle={true}
-                  angle={140}>
-                  <View
-                    style={[
-                      {
-                        borderRadius: 40,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                      },
-                      focused
-                        ? {height: 80, width: 80}
-                        : {
-                            height: 40,
-                            width: 40,
-                            alignSelf: 'flex-end',
-                          },
-                    ]}>
-                    <Awesome5Icon name="search" size={25} color={color} solid />
-                  </View>
-                </LinearGradient>
-              </TouchableOpacity>
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </View>
+    <Tab.Navigator
+      initialRouteName="HomePage"
+      screenOptions={{
+        headerShown: false,
+        tabBarBackground: () => <Background />,
+        tabBarInactiveTintColor: 'white',
+        tabBarActiveTintColor: 'white',
+        tabBarStyle: {
+          height: 80,
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          paddingTop: 15,
+        },
+      }}
+      sceneContainerStyle={{flex: 1}}>
+      <Tab.Screen
+        name="HomePage"
+        component={Home}
+        options={{
+          title: '',
+          tabBarIcon: ({color, focused}) => (
+            <TouchableOpacity onPress={() => console.log('hola')}>
+              <LinearGradient
+                colors={focused ? ['#60FFCA', '#19423400'] : ['transparent']}
+                style={{borderRadius: 40, padding: 1}}
+                useAngle={true}
+                angle={140}>
+                <View
+                  style={[
+                    {
+                      borderRadius: 40,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    },
+                    focused
+                      ? {height: 80, width: 80}
+                      : {
+                          height: 40,
+                          width: 40,
+                        },
+                  ]}>
+                  <Icon name="house" size={25} color={color} solid />
+                </View>
+              </LinearGradient>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SeachPage"
+        component={() => <></>}
+        options={{
+          title: '',
+          tabBarIcon: ({color, focused}) => (
+            <TouchableOpacity onPress={() => console.log('hola')}>
+              <LinearGradient
+                colors={focused ? ['#60FFCA', '#19423400'] : ['transparent']}
+                style={{borderRadius: 40, padding: 1}}
+                useAngle={true}
+                angle={140}>
+                <View
+                  style={[
+                    {
+                      borderRadius: 40,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    },
+                    focused
+                      ? {height: 80, width: 80}
+                      : {
+                          height: 40,
+                          width: 40,
+                          alignSelf: 'flex-end',
+                        },
+                  ]}>
+                  <Awesome5Icon name="search" size={25} color={color} solid />
+                </View>
+              </LinearGradient>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
